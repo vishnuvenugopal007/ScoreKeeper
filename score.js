8f3fd25e1b546bbfbe40543f08e72c78a6f29a29
vishnuvenugopal007 <vishnu.venugopal@live.com>
2017-05-24 13:28:59 -0700

    User can increment and reset score

---
+++ score.js
@@ -0,0 +1,45 @@
+var p1button = document.getElementById('p1')
+var p2button = document.getElementById('p2')
+
+var p1Display = document.getElementById('p1-display')
+var p2Display = document.getElementById('p2-display')
+
+var p1Score = 0
+var p2Score = 0
+
+var gameOver = false
+var winningScore = document.getElementById('winning-score')
+
+var reset = document.getElementById('reset')
+
+p1button.addEventListener('click', function() {
+  if(!gameOver){
+    p1Score++
+    if(p1Score === winningScore){
+      p1Display.classList.add('winner')
+      gameOver = true
+    }
+    p1Display.textContent = p1Score
+  }
+})
+
+p2button.addEventListener('click', function() {
+  if(!gameOver){
+    p2Score++
+    if(p2Score === winningScore){
+      p2Display.classList.add('winner')
+      gameOver = true
+    }
+    p2Display.textContent = p2Score
+  }
+})
+
+reset.addEventListener('click', function() {
+    p1Score = 0
+    p2Score = 0
+    p1Display.textContent = p1Score
+    p2Display.textContent = p2Score
+    p1Display.classList.remove('winner')
+    p2Display.classList.remove('winner')
+    gameOver = false
+})
